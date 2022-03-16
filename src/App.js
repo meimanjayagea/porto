0cb56c1e651185e24aea47fecc516b975353bde0
meimanjayagea <jayameiman17@gmail.com>
2022-03-16 22:02:03 +0700

    install bootstrap 5.1.3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
     <div>
-     <h1>Selamat Datang</h1>
+     <h1 >Selamat Datang</h1>
+     <button className="btn btn-primary">Selamat Datang</button>
     </div>
   );
 }
